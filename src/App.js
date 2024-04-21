3b64c7e81a1fb967c980beebb3518fb95e3682c5
Pedrolivares1980 <pedr.olivares@yahoo.com>
2024-04-21 15:31:54 +0100

    navigation and background

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./components/nav/Nav.js";
+import About from "./pages/about/About.js"
+import Skills from "./pages/skills/Skills.js"
+import Projects from "./pages/projects/Projects.js"
+import Contact from "./pages/contact/Contact.js"
+import "./styles/app.css";
+import Background from "./background/Background.js";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav />
+      <Background />
+      <Routes>
+        <Route path="/" element={<About/>} />
+        <Route path="/skills" element={<Skills/>} />
+        <Route path="/projects" element={<Projects/>} />
+        <Route path="/contact" element={<Contact/>} />
+      </Routes>
+    </Router>
   );
 }
 
